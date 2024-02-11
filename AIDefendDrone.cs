354a80a0d22e12a38d883434d62108233b96fb87
alexnew1999 <153484098+alexnew1999@users.noreply.github.com>
2024-02-11 21:27:54 +0100

    Add files via upload

---
+++ AIDefendDrone.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDefendDrone : MonoBehaviour
+{
+	public CharacterData characterData;
+	public CellData cityCell;
+	public CharacterData targetCharacter;
+	public CharacterMovementAI characterMovementAI;
+	public CharacterAttackAI characterAttackAI;
+}
